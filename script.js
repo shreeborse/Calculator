352bfb2d96d8acd62047fcdf836549f515142bee
shreeborse <145749000+shreeborse@users.noreply.github.com>
2023-09-22 12:31:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+javascript
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    const expression = document.getElementById('display').value;
+    const result = eval(expression);
+    document.getElementById('display').value = result;
+}
